d336216fa73f8095fdf8f2eba5bd63800ec01047
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-07-11 14:50:51 +0200

    - cloudiolab main page

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import siteImage from '../img/logo.png'
 
 class Landing extends Component {
     render() {
@@ -7,6 +8,13 @@ class Landing extends Component {
             <div className="container">
                 <div className="text-dark">
                     <div className="content mb-4">
+                        <h1 className="name text-center mt-5">Cloud<img src={siteImage} alt="Site logo" />lab</h1>
+                        <hr />
+                        <div className="text-center">
+                            <p>Greetings from cloud laboratory.</p>
+                            <p>The person who builds this laboratory is working on something.</p>
+                            <p>Please tell him to takecare of me, if you see this guy. And don't forget to <a href="https://me.cloudiolab.com" target="_blank" rel="noopener noreferrer">Hit Him!</a>.</p>
+                        </div>
                     </div>
                 </div >
             </div >
