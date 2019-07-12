afb0a3010fb985f21b05e2ef3b91a6147ed00639
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-07-12 15:01:00 +0200

    - made finalized version of cloudiolab.com

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -7,13 +7,68 @@ class Landing extends Component {
         return (
             <div className="container">
                 <div className="text-dark">
-                    <div className="content mb-4">
+                    <div className="content mb-5">
                         <h1 className="name text-center mt-5">Cloud<img src={siteImage} alt="Site logo" />lab</h1>
                         <hr />
-                        <div className="text-center">
-                            <p>Greetings from cloud laboratory.</p>
-                            <p>The person who builds this laboratory is working on something.</p>
-                            <p>Please tell him to takecare of me, if you see this guy. And don't forget to <a href="https://me.cloudiolab.com" target="_blank" rel="noopener noreferrer">Hit Him!</a>.</p>
+                        <div className="text-center mt-4 mb-4">
+                            <p className="font-italic">Greetings from cloud laboratory.</p>
+                            <p>A full stack developler and his presonal cloud laboratory to play with swarm and kubernetes cluster.</p>
+                            <p>A person who made this laboratory is working on something.</p>
+                            <p>Please tell him to clean the laboratory (Codebase), if you see this guy. And don't forget to <a href="https://me.cloudiolab.com" target="_blank" rel="noopener noreferrer"><i>Hit Him!</i></a>.</p>
+                        </div>
+                        <hr />
+                        <p className="text-center font-weight-bold mt-4 mb-4">Everything you see here is running on either SWARM / Kubernetes cluster</p>
+                        <div class="card-deck mt-4">
+                            <div className="card text-center">
+                                <div className="card-body">
+                                    <h5 className="card-title">Private Docker Registry</h5>
+                                    <h6 className="card-subtitle mb-2 text-muted">In Production</h6>
+                                    <p className="card-text">Gives freedom to share your custom base images within organization to keep consistent, private and centralized source of your images.</p>
+                                    <a className="card-link" href="https://docker.cloudiolab.com" target="_blank" rel="noopener noreferrer">Need a free ride?</a>
+                                    <a className="card-link" href="https://github.com/vesanthosh/docker_cloudiolab" target="_blank" rel="noopener noreferrer">Github</a>
+                                </div>
+                            </div>
+                            <div className="card text-center">
+                                <div className="card-body">
+                                    <h5 className="card-title">Do-Things</h5>
+                                    <h6 className="card-subtitle mb-2 text-muted">In Development</h6>
+                                    <p className="card-text">Share your tasks / goals and let others knows your social activities. Get help or help others through social task sharing platform.</p>
+                                    <a className="card-link" href="https://dothingsprivate.herokuapp.com/" target="_blank" rel="noopener noreferrer">Need a free ride?</a>
+                                    <a className="card-link" href="https://github.com/vesanthosh/dothings-cloudiolab" target="_blank" rel="noopener noreferrer">Github</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-deck mt-4 mb-4">
+                            <div className="card bg-light text-center">
+                                <div className="card-body">
+                                    <h5 className="card-title">Cricket Planner</h5>
+                                    <h6 className="card-subtitle mb-2 text-muted">Coming Soon...</h6>
+                                    <p className="card-text">Find the right time to play that suits everyone. Predict the optimal date and time about who is interested.</p>
+                                    <a className="card-link" href="https://cricket.cloudiolab.com" target="_blank" rel="noopener noreferrer">Need a free ride?</a>
+                                    <a className="card-link" href="https://github.com/vesanthosh/cricket_cloudiolab" target="_blank" rel="noopener noreferrer">Github</a>
+                                </div>
+                            </div>
+                            <div className="card bg-light text-center">
+                                <div className="card-body">
+                                    <h5 className="card-title">Cluster Monitoring</h5>
+                                    <h6 className="card-subtitle mb-2 text-muted">Brainstorming</h6>
+                                    <p className="card-text">Dashboard that tracks and monitors container, node's metrics and perfomance from SWARM / Kubernetes cluster. </p>
+                                    {/* <a href="#" className="card-link">Card link</a>
+                                    <a href="#" className="card-link">Another link</a> */}
+                                </div>
+                            </div>
+                        </div>
+
+                        <hr />
+                        <div class="card-deck mt-4">
+                            <div className="card text-center">
+                                <div className="card-body">
+                                    <h5 className="card-title">About Me</h5>
+                                    <h6 className="card-subtitle mb-2 text-muted">In Production</h6>
+                                    <a className="card-link" href="https://me.cloudiolab.com" target="_blank" rel="noopener noreferrer">Wanna know?</a>
+                                    <a className="card-link" href="https://me.cloudiolab.com/resume" target="_blank" rel="noopener noreferrer">Career history?</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div >
